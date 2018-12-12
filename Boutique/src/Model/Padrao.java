e537be82ab26f289dc4666d7f5f42a1ea2fa00d9
annaKarolinePrado <annakaroline9298@gmail.com>
2018-12-12 20:41:04 -0200

    Criado as model Padrão e Estado

---
+++ Boutique/src/Model/Padrao.java
@@ -0,0 +1,59 @@
+
+package Model;
+
+abstract public class Padrao {
+    private int id;
+    private String nome;
+    
+    public Padrao(){
+        
+    }    
+    public Padrao(int id, String nome){
+        this.setId(id);
+        this.setNome(nome);
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return  nome;
+    }   
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Padrao other = (Padrao) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }   
+}
